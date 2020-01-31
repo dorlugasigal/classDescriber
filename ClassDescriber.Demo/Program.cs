453f5476a5055f5572156ff9b22f54c6f80d93a9
Dor Lugasi-Gal <dorlugasigal@gmail.comgit>
2020-01-31 11:38:35 +0200

    update

--- ClassDescriber.Demo/Program.cs
+++ ClassDescriber.Demo/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using ClassDescriber.Library;
 
 namespace ClassDescriber.Demo
 {
@@ -6,7 +9,24 @@ namespace ClassDescriber.Demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var per1 = new Person { Name = "Dor1", address = new Address { Number = 1, Street = "haha1" } };
+            var per2 = new Person { Name = "Dor2", address = new Address { Number = 2, Street = "haha2" } };
+            var per3 = new Person { Name = "Dor3", address = new Address { Number = 3, Street = "haha3" } };
+            IEnumerable<Person> arr = new List<Person> { per1, per2, per3 };
+            Console.Write(Describer.Describe(arr));
         }
+
+
+
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+        public Address address { get; set; }
+    }
+    public class Address
+    {
+        public string Street { get; set; }
+        public int Number { get; set; }
     }
 }
--- ClassDescriber.Library/Describer.cs
+++ ClassDescriber.Library/Describer.cs
@@ -1,12 +1,165 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
 
 namespace ClassDescriber.Library
 {
     public class Describer
     {
-        public static void Describe(object obj)
+        private int _level;
+        private const int IndentationSize = 2;
+        private readonly StringBuilder _output;
+        private readonly HashSet<object> _propsUsed;
+
+        private Describer()
         {
-            if (obj == null) throw new ArgumentNullException(nameof(obj));
+            _output = new StringBuilder();
+            _propsUsed = new HashSet<object>();
+        }
+
+        public static string Describe(object element)
+        {
+            var instance = new Describer();
+            return instance.DescribeElement(element);
+        }
+
+        private string DescribeElement(object element)
+        {
+            if (IsSimple(element))
+            {
+                Write(FormatValue(element));
+            }
+            else
+            {
+                var objectType = element.GetType();
+                if (!typeof(IEnumerable).IsAssignableFrom(objectType))
+                {
+                    Write($"{objectType.Name}");
+                    _propsUsed.Add(element);
+                    _level++;
+                }
+
+                if (element is IEnumerable enumerableElement)
+                {
+                    foreach (var item in enumerableElement)
+                    {
+                        if (item is IEnumerable && !(item is string))
+                        {
+                            _level++;
+                            DescribeElement(item);
+                            _level--;
+                        }
+                        else
+                        {
+                            if (!AlreadyTouched(item))
+                            {
+                                DescribeElement(item);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    var members = element.GetType().GetMembers(BindingFlags.Public | BindingFlags.Instance);
+                    foreach (var memberInfo in members)
+                    {
+                        var fieldInfo = memberInfo as FieldInfo;
+                        var propertyInfo = memberInfo as PropertyInfo;
+
+                        if (fieldInfo == null && propertyInfo == null)
+                        {
+                            continue;
+                        }
+
+                        var type = fieldInfo != null ? fieldInfo.FieldType : propertyInfo.PropertyType;
+                        var value = fieldInfo != null
+                            ? fieldInfo.GetValue(element)
+                            : propertyInfo.GetValue(element, null);
+
+                        if (type.IsValueType || type == typeof(string))
+                        {
+                            Write($"{memberInfo.Name}: {FormatValue(value)}");
+                        }
+                        else
+                        {
+                            var isEnumerable = typeof(IEnumerable).IsAssignableFrom(type);
+                            var addition = isEnumerable ? "..." : "{ }";
+                            Write($"{memberInfo.Name}: {addition}");
+
+                            var alreadyTouched = !isEnumerable && AlreadyTouched(value);
+                            _level++;
+                            if (!alreadyTouched)
+                            {
+                                DescribeElement(value);
+                            }
+
+                            _level--;
+                        }
+                    }
+                }
+
+                if (!typeof(IEnumerable).IsAssignableFrom(objectType))
+                {
+                    _level--;
+                }
+            }
+
+            return _output.ToString();
+        }
+
+        private static bool IsSimple(object element)
+        {
+            return element == null || element is ValueType || element is string;
+        }
+
+        private bool AlreadyTouched(object value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+
+            foreach (var t in _propsUsed)
+            {
+                if (t == value)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void Write(string value, params object[] args)
+        {
+            var space = new string(' ', _level * IndentationSize);
+
+            if (args != null)
+                value = string.Format(value, args);
+
+            _output.AppendLine(space + value);
+        }
+
+        private static string FormatValue(object o)
+        {
+            switch (o)
+            {
+                case null:
+                    return ("null");
+                case DateTime time:
+                    return (time.ToShortDateString());
+                case string _:
+                    return $"\"{o}\"";
+                case char c when c == '\0':
+                    return string.Empty;
+                case ValueType _:
+                    return (o.ToString());
+                case IEnumerable _:
+                    return ("...");
+                default:
+                    return ("{ }");
+            }
         }
     }
 }
